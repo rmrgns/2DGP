83549427de083c4627697dd70eefdb942cfe53a4
wjdrmrgns <rmrgns@naver.com>
2024-11-10 23:12:06 +0900

    Create main_scene.py
    메인씬을 만들고 시작씬으로 변경

---
+++ code/main_scene.py
@@ -0,0 +1,44 @@
+from pico2d import *
+import gfw
+import game_scene
+
+world = gfw.World(['mainbg'])
+canvas_width = 500
+canvas_height = 800
+
+def enter():
+    pass
+
+def exit():
+    world.clear()
+    print('[main.exit()]')
+
+def pause():
+    print('[main.pause()]')
+
+def resume():
+    print('[main.resume()]')
+
+def handle_event(e):
+    if e.type == SDL_KEYDOWN:
+        print(world.objects)
+        gfw.start(game_scene)
+
+
+class CollisionChecker:
+    def draw(self): pass
+    def update(self):
+        pass
+
+class GameScenUI:
+    def __init__(self):
+        self.font = load_font('res/lucon.ttf', 50)
+        self.pos = (canvas_width - 320, canvas_height - 40)
+    def update(self): pass
+    def draw(self):
+        #self.font.draw(*self.pos, f'{score:10d}')
+        pass
+
+
+if __name__ == '__main__':
+    gfw.start_main_module()
