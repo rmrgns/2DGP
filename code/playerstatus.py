25a8add4922a67341056e78aa841ca63787cc894
wjdrmrgns <rmrgns@naver.com>
2024-12-09 13:45:55 +0900

    Create playerstatus.py
    플레이어 스탯을 저장할 playerstatus 클래스 추가

---
+++ code/playerstatus.py
@@ -0,0 +1,18 @@
+from pico2d import *
+import gfw
+
+class PlayerStatus():
+    def __init__(self):
+        # resource
+        gold = 0
+
+        # score
+        score = 0
+
+        # Upgrade
+        fighterHPUpgrade = 0
+        shieldturretHPUpgrade = 0
+        gunturretATKUpgrade = 0
+
+
+    pass
